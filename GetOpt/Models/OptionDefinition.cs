9ab225c3c3eb85f8c6e7f2e09c37c3272ad8b137
Christoph Hochstaetter <Christoph@Hochstaetter.de>
2020-05-24 05:41:20 +0200

    Documented OptionDefinition

--- GetOpt/Models/OptionDefinition.cs
+++ GetOpt/Models/OptionDefinition.cs
@@ -6,20 +6,48 @@ namespace De.Hochstaetter.CommandLine.Models
     {
         public string LongName { get; }
         public char ShortName { get; }
-        public bool HasArgument => ArgumentType != null;
+        public bool HasArgument => !(ArgumentType is null);
         public Type ArgumentType { get; }
         public dynamic Minimum { get; }
         public dynamic Maximum { get; }
         public object Tag { get; }
 
-        public OptionDefinition(string longName, string shortName = default, Type argumentType = null) :
-            this(longName, StringToChar(shortName), argumentType)
-        { }
+        /// <summary>
+        /// Creates an option that does not have an argument
+        /// </summary>
+        /// <param name="longName">The long name for the option for instance --use-ssl</param>
+        /// <param name="shortName">The single-character short name for the option for instance -s</param>
+        public OptionDefinition(string longName, string shortName = default) : this(longName, StringToChar(shortName), null) { }
 
+        /// <summary>
+        /// Creates an option that does not have an argument
+        /// </summary>
+        /// <param name="longName">The long name for the option for instance --use-ssl</param>
+        /// <param name="shortName">The short name for the option for instance -s</param>
+        public OptionDefinition(string longName, char shortName = default) : this(longName, shortName, null) { }
+
+        /// <summary>
+        /// Creates an option that requires an argument
+        /// </summary>
+        /// <param name="longName">The long name for the option for instance --use-ssl</param>
+        /// <param name="shortName">The single-character short name for the option for instance -s</param>
+        /// <param name="argumentType">The type (e.g. <see cref="int"/>) that the option argument should be
+        /// converted to. If you don't want any conversion use <see cref="string"/></param>
+        /// <param name="minimum">The minimum value allowed for this option. Should be of the type as <paramref name="argumentType"/></param>
+        /// <param name="maximum">The maximum value allowed for this option. Should be of the type as <paramref name="argumentType"/></param>
         public OptionDefinition(string longName, string shortName, Type argumentType, object minimum = null, object maximum = null, object tag = null) :
             this(longName, StringToChar(shortName), argumentType, minimum, maximum, tag)
         { }
 
+        /// <summary>
+        /// Creates an option that requires an argument
+        /// </summary>
+        /// <param name="longName">The long name for the option for instance --use-ssl</param>
+        /// <param name="shortName">The short name for the option for instance -s</param>
+        /// <param name="argumentType">The type (e.g. <see cref="int"/>) that the option argument should be
+        /// converted to. If you don't want any conversion use <see cref="string"/></param>
+        /// <param name="minimum">The minimum value allowed for this option. Should be of the type as <paramref name="argumentType"/></param>
+        /// <param name="maximum">The maximum value allowed for this option. Should be of the type as <paramref name="argumentType"/></param>
         public OptionDefinition(string longName, char shortName, Type argumentType, dynamic minimum = null, dynamic maximum = null, object tag = null)
         {
             if (shortName == default && longName == default)
@@ -30,6 +58,11 @@ namespace De.Hochstaetter.CommandLine.Models
             if (minimum != null && maximum != null && minimum > maximum)
             {
                 throw new ArgumentException($"{nameof(Maximum)} must be equal or greater to {nameof(Minimum)}");
+            }
+
+            if (ArgumentType == null && (!(Minimum is null) || !(Maximum is null)))
+            {
+                throw new ArgumentException("Options without an argument can have neither minimum nor maximum");
             }
 
             ShortName = shortName;
@@ -56,9 +89,14 @@ namespace De.Hochstaetter.CommandLine.Models
 
         private static char StringToChar(string shortName)
         {
+            if (string.IsNullOrEmpty(shortName))
+            {
+                return default;
+            }
+
             if (shortName.Length != 1)
             {
-                throw new ArgumentException("Short option must be one character only", nameof(shortName));
+                throw new ArgumentException("Short option must have exactly one character", nameof(shortName));
             }
 
             return shortName[0];
--- GetOptTests/TestOptions.cs
+++ GetOptTests/TestOptions.cs
@@ -7,12 +7,29 @@ namespace De.Hochstaetter.GetOptTests
     {
         public static IEnumerable<OptionDefinition> Standard = new[]
         {
-            new OptionDefinition("verbose", "v"),
-            new OptionDefinition("max-workers", 'W' ,typeof(int), 1, 10),
-            new OptionDefinition("debug-level", 'd' ,typeof(byte), 0, 4),
-            new OptionDefinition("max-cpu-share", 'c' ,typeof(double), 0, 1),
-            new OptionDefinition("work-day", "w", typeof(WeekDay), WeekDay.Monday, WeekDay.Friday),
-            new OptionDefinition("show-minor-errors", 'e', typeof(bool)), 
+            // Option without argument
+            new OptionDefinition(longName:"verbose", shortName:"v"),
+
+            // Option with uint argument and minimum 1 (no maximum except uint.MaxValue)
+            new OptionDefinition(longName:"max-workers", shortName:'W', typeof(uint), minimum: 1),
+
+            // Option with byte argument and restricted from 0 to 4
+            new OptionDefinition(longName:"debug-level", shortName:'d', typeof(byte), minimum:0, maximum:4),
+
+            // Option with double argument and restricted from 0 to 1
+            new OptionDefinition(longName:"max-cpu-share", shortName:'c', typeof(double), minimum:0, maximum:1),
+
+            // Option with enum argument and restricted from Monday to Friday
+            new OptionDefinition(longName:"work-day", shortName:"w", typeof(WeekDay), minimum:WeekDay.Monday, maximum:WeekDay.Friday),
+
+            // Option with bool argument
+            new OptionDefinition(longName:"show-minor-errors", shortName:'e', typeof(bool)),
+
+            // Option with string argument
+            new OptionDefinition(longName:"log-file", shortName:'l', typeof(string)),
+
+            // Option with string argument, that has no short form
+            new OptionDefinition(longName:"first-name", shortName:null, typeof(string)),
         };
     }
 }
