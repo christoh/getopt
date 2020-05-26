1b00572355b39fa0eafe1ae42e469ce3ea15c312
Christoph Hochstaetter <Christoph@Hochstaetter.de>
2020-05-26 03:00:16 +0200

    Created separate settings class in demo usage

--- demo/Program.cs
+++ demo/Program.cs
@@ -8,7 +8,7 @@ using De.Hochstaetter.CommandLine.Models;
 
 namespace De.Hochstaetter.Commandline.Demo
 {
-    internal class Program
+    public class Settings
     {
         private const string VerboseHelp = "Show verbose output";
         private const string LogFileHelp = "Write log to <file>";
@@ -20,23 +20,27 @@ namespace De.Hochstaetter.Commandline.Demo
                                           "\n\tFirst char must be upper-case letter. Can be used multiple times.";
 
         [GetOpt(LongName = "verbose", ShortName = 'v', HasArgument = false, Help = VerboseHelp)]
-        public static bool Verbose;
+        public bool Verbose;
 
         [GetOpt(LongName = "help", ShortName = 'h', HasArgument = false, Help = HelpHelp)]
-        public static bool ShowHelp;
+        public bool ShowHelp;
 
         [GetOpt(LongName = "log-file", ShortName = 'l', ArgumentName = "<file>", Help = LogFileHelp)]
-        public static string LogFile { get; set; }
+        public string LogFile { get; set; }
 
         [GetOpt(LongName = "notify-email", ShortName = 'n', ArgumentName = "<email-address>", Help = NotifyHelp)]
-        public static ISet<string> EmailNotifications { get; } = new HashSet<string>();
+        public ISet<string> EmailNotifications { get; } = new HashSet<string>();
 
         [GetOpt(LongName = "work-day", ShortName = 'w', Minimum = DayOfWeek.Monday, Maximum = DayOfWeek.Friday, ArgumentName = "<Monday..Friday>", Help = WorkDayHelp)]
-        private static DayOfWeek? WorkDay;
+        public DayOfWeek? WorkDay;
 
         [GetOpt(LongName = "tag", ShortName = 't', RegexPattern = "^[A-Z][A-Z0-9_]{0,7}$", ArgumentName = "<job-tag>", Help = JobTagHelp)]
-        public static List<string> JobTags { get; set; }
+        public List<string> JobTags { get; set; }
 
+    }
+
+    internal class Program
+    {
         internal static void Main(string[] args)
         {
             Console.WriteLine($"demo - a program that has many command line options - version {Assembly.GetExecutingAssembly().GetName().Version}");
@@ -44,7 +48,9 @@ namespace De.Hochstaetter.Commandline.Demo
             Console.WriteLine($"Command line: {Environment.CommandLine}");
             Console.WriteLine();
 
-            var getOpt = new GetOpt(typeof(Program));
+            var settings = new Settings();
+            var getOpt = new GetOpt(settings);
+
             ParsedArguments parsedArguments;
 
             try
@@ -57,18 +63,18 @@ namespace De.Hochstaetter.Commandline.Demo
                 return;
             }
 
-            if (ShowHelp)
+            if (settings.ShowHelp)
             {
                 Usage(getOpt);
                 return;
             }
 
             Console.WriteLine("Job files to run     : {0}", string.Join(", ", parsedArguments.NonOptions));
-            Console.WriteLine("Run job              : {0}", WorkDay.HasValue ? (object)WorkDay.Value : "ASAP");
-            Console.WriteLine("Log goes to to       : {0}", LogFile);
-            Console.WriteLine("Be verbose           : {0}", Verbose ? "yes" : "no");
-            Console.WriteLine("Send notification to : {0}", string.Join(", ", EmailNotifications));
-            Console.WriteLine("Job tags             : {0}", string.Join(", ", JobTags));
+            Console.WriteLine("Run job              : {0}", settings.WorkDay.HasValue ? (object)settings.WorkDay.Value : "ASAP");
+            Console.WriteLine("Log goes to to       : {0}", settings.LogFile);
+            Console.WriteLine("Be verbose           : {0}", settings.Verbose ? "yes" : "no");
+            Console.WriteLine("Send notification to : {0}", string.Join(", ", settings.EmailNotifications));
+            Console.WriteLine("Job tags             : {0}", string.Join(", ", settings.JobTags));
         }
 
         private static void Usage(GetOpt getOpt)
